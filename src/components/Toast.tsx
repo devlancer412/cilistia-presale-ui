aa45f0e875b252365b56412a6e3d9cd15a7517f0
Wai <wai1chung@gmail.com>
2023-03-20 21:25:35 +0800

    fix: adjust toast position

--- src/components/Toast.tsx
+++ src/components/Toast.tsx
@@ -1,15 +1,22 @@
-import { Toaster, ToastOptions } from "react-hot-toast";
+import { Toaster, ToastOptions } from 'react-hot-toast';
 
 const toastOptions: ToastOptions = {
   style: {
-    padding: "16px",
-    background: "rgb(255 255 255 / 0.05)",
-    color: "#FFFFFF",
+    padding: '16px',
+    background: 'rgb(31, 29, 43, 0.95)',
+    color: '#FFFFFF',
+    width: 'fit-content',
   },
 } as ToastOptions;
 
 const Toast = () => {
-  return <Toaster toastOptions={toastOptions} />;
+  return (
+    <Toaster
+      containerStyle={{ top: 50 }}
+      position='top-right'
+      toastOptions={toastOptions}
+    />
+  );
 };
 
 export default Toast;
