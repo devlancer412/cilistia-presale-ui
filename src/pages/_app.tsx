f53013dc95559cc89153663d03845c3588ac0f72
Wai <wai1chung@gmail.com>
2023-03-22 16:27:38 +0800

    fix: wrap state tree with suspense to prevent SSR

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,34 +1,35 @@
-import "@/styles/globals.css";
-import "@rainbow-me/rainbowkit/styles.css";
-import type { AppProps } from "next/app";
+import '@/styles/globals.css';
+import '@rainbow-me/rainbowkit/styles.css';
+import { Suspense } from 'react';
+import type { AppProps } from 'next/app';
 import {
   RainbowKitProvider,
   getDefaultWallets,
   connectorsForWallets,
   darkTheme,
-} from "@rainbow-me/rainbowkit";
+} from '@rainbow-me/rainbowkit';
 import {
   argentWallet,
   trustWallet,
   ledgerWallet,
-} from "@rainbow-me/rainbowkit/wallets";
-import { configureChains, createClient, WagmiConfig } from "wagmi";
-import { goerli, arbitrum } from "wagmi/chains";
-import { publicProvider } from "wagmi/providers/public";
-import { alchemyProvider } from "wagmi/providers/alchemy";
+} from '@rainbow-me/rainbowkit/wallets';
+import { configureChains, createClient, WagmiConfig } from 'wagmi';
+import { goerli, arbitrum } from 'wagmi/chains';
+import { publicProvider } from 'wagmi/providers/public';
+import { alchemyProvider } from 'wagmi/providers/alchemy';
 
-import { Navbar, Footer } from "@/components/Layout";
-import ContextProviders from "@/contexts";
-import ErrorBoundary from "@/components/ErrorBoundary";
-import Toast from "@/components/Toast";
+import { Navbar, Footer } from '@/components/Layout';
+import ContextProviders from '@/contexts';
+import ErrorBoundary from '@/components/ErrorBoundary';
+import Toast from '@/components/Toast';
 
-const APP_NAME = "CIL Presale";
+const APP_NAME = 'CIL Presale';
 const ALCHEMY_API_KEY =
-  process.env.NEXT_PUBLIC_ALCHEMY_KEY ?? "gRk6Rk7lqJsLeybRhCGlyL2LMGD8-CLf";
+  process.env.NEXT_PUBLIC_ALCHEMY_KEY ?? 'gRk6Rk7lqJsLeybRhCGlyL2LMGD8-CLf';
 
 const { chains, provider, webSocketProvider } = configureChains(
   [
-    ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === "true"
+    ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === 'true'
       ? [arbitrum]
       : [goerli]),
   ],
@@ -53,7 +54,7 @@ const demoAppInfo = {
 const connectors = connectorsForWallets([
   ...wallets,
   {
-    groupName: "Other",
+    groupName: 'Other',
     wallets: [
       argentWallet({ chains }),
       trustWallet({ chains }),
@@ -74,36 +75,38 @@ const wagmiClient = createClient({
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ErrorBoundary>
-      <WagmiConfig client={wagmiClient}>
-        <RainbowKitProvider
-          coolMode
-          theme={darkTheme({
-            accentColor: "#6366f1",
-            accentColorForeground: "white",
-            borderRadius: "small",
-            fontStack: "system",
-            overlayBlur: "small",
-          })}
-          modalSize="compact"
-          appInfo={demoAppInfo}
-          chains={chains}
-        >
-          <ContextProviders>
-            <main className="flex flex-col min-h-screen overflow-hidden bg-gray-900">
-              <Navbar />
+    <Suspense>
+      <ErrorBoundary>
+        <WagmiConfig client={wagmiClient}>
+          <RainbowKitProvider
+            coolMode
+            theme={darkTheme({
+              accentColor: '#6366f1',
+              accentColorForeground: 'white',
+              borderRadius: 'small',
+              fontStack: 'system',
+              overlayBlur: 'small',
+            })}
+            modalSize='compact'
+            appInfo={demoAppInfo}
+            chains={chains}
+          >
+            <ContextProviders>
+              <main className='flex flex-col min-h-screen overflow-hidden bg-gray-900'>
+                <Navbar />
 
-              {/*  Page content */}
-              <div className="grow z-10">
-                <Component {...pageProps} />
-              </div>
+                {/*  Page content */}
+                <div className='grow z-10'>
+                  <Component {...pageProps} />
+                </div>
 
-              <Footer />
-              <Toast />
-            </main>
-          </ContextProviders>
-        </RainbowKitProvider>
-      </WagmiConfig>
-    </ErrorBoundary>
+                <Footer />
+                <Toast />
+              </main>
+            </ContextProviders>
+          </RainbowKitProvider>
+        </WagmiConfig>
+      </ErrorBoundary>
+    </Suspense>
   );
 }
