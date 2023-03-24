828d77eeb8fda6698742eb045e5f7b7300f6954e
WaiChung <wai1chung@gmail.com>
2023-03-25 01:15:00 +0800

    feat: update subgraph url

--- src/constants/subgraph.ts
+++ src/constants/subgraph.ts
@@ -1,6 +1,6 @@
 export const SUBGRAPH_ENDPOINTS: Record<number, string> = {
   42161:
-    'https://api.thegraph.com/subgraphs/name/devlancer412/cilistia-subgraph-goerli',
+    'https://api.thegraph.com/subgraphs/name/devlancer412/cilistia-arbitrum',
   1337: 'https://api.thegraph.com/subgraphs/name/devlancer412/cilistia-subgraph-goerli',
   5: 'https://api.thegraph.com/subgraphs/name/devlancer412/cilistia-subgraph-goerli',
   1: 'https://api.thegraph.com/subgraphs/name/devlancer412/cilistia-subgraph-goerli',
