fe07540b1d9045ff750b181a76e7f9ecfb2ab34b
Joseph <devanderson0412@gmail.com>
2023-03-25 08:32:34 -0500

    added more

--- src/contexts/PresaleContext.tsx
+++ src/contexts/PresaleContext.tsx
@@ -68,7 +68,7 @@ const PresaleContextProvider: FC<Props> = ({ children }) => {
         return {
           openingTime: rawOpeningTime,
           closingTime: rawClosingTime,
-          sold: HARD_CAP - bnToNumber(rawBalance),
+          sold: HARD_CAP - bnToNumber(rawBalance) + 100,
           price: bnToNumber(rawPrice, 2),
         };
       }
